c32d58b2ec3eabf120a19e6adae09a61c8b4637a
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-10-25 12:57:12 -0500

    added routes to connect to api by id and by query- tested in postman with auth middleware

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+const axios = require('axios')
 
 //extra security packages
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
-
+const apiKey = process.env.apiKey
 // Swagger
 //const swaggerUI = require('swagger-ui-express');
 //const YAML = require('yamljs');
@@ -40,15 +41,9 @@ app.use(xss());
 
 
 // extra packages
-
-;// routes
-// app.get('/', (req, res) => {
-//   res.send('jobs api');
-// })
 // app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/api/v1/auth', authRouter)
-// app.use('/api/v1/jobs', auth)
 
 app.get('/', (req, res) => {
   res.status(200).send('testing route is good')
@@ -57,6 +52,25 @@ app.get('/', (req, res) => {
 app.get('/checkUser', auth, (req, res) => {
   res.status(200).send(`user ${req.user.name} is logged in`)
 })
+
+//get recipe by query- combines searching by query, by ingredients, and by nutrients into one endpoint.
+//this is the 'search recipe' endpoint in the spoonacular docs
+app.get("/api/v1/recipes", auth, async (req,res) => {
+  req.query.apiKey = apiKey;
+  const results = await axios.get("https://api.spoonacular.com/recipes/complexSearch", {params: req.query});
+  console.log(results);
+  res.status(results.status).send(results.data) ;
+
+});
+//get recipe by id- 
+//get full information about a recipe by recipe id, such as ingredients, nutrition, diet and allergen information, etc
+app.get("/api/v1/recipes/:id", auth, async (req,res) => {
+  req.query.apiKey = apiKey;
+  const results = await axios.get(`https://api.spoonacular.com/recipes/${req.params.id}/information`, {params: req.query});
+  console.log(results);
+  res.status(results.status).send(results.data) ;
+
+  });
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
