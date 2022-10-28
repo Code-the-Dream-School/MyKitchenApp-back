99667cd62937d42dd9ed3de8d92f769507af997c
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-10-28 13:44:26 -0500

    added routes & controllers file and moved the two routes appropriately

--- app.js
+++ app.js
@@ -22,11 +22,12 @@ const auth = require('./middleware/authentication')
 
 //routers
 const authRouter = require('./routes/auth')
-
+const recipes = require('./routes/recipe')
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
 
 //app.set('trust proxy', 1);
 app.use(rateLimiter({
@@ -44,6 +45,9 @@ app.use(xss());
 // app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/recipes', auth, recipes)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 app.get('/', (req, res) => {
   res.status(200).send('testing route is good')
@@ -53,27 +57,6 @@ app.get('/checkUser', auth, (req, res) => {
   res.status(200).send(`user ${req.user.name} is logged in`)
 })
 
-//get recipe by query- combines searching by query, by ingredients, and by nutrients into one endpoint.
-//this is the 'search recipe' endpoint in the spoonacular docs
-app.get("/api/v1/recipes", auth, async (req,res) => {
-  req.query.apiKey = apiKey;
-  const results = await axios.get("https://api.spoonacular.com/recipes/complexSearch", {params: req.query});
-  console.log(results);
-  res.status(results.status).send(results.data) ;
-
-});
-//get recipe by id- 
-//get full information about a recipe by recipe id, such as ingredients, nutrition, diet and allergen information, etc
-app.get("/api/v1/recipes/:id", auth, async (req,res) => {
-  req.query.apiKey = apiKey;
-  const results = await axios.get(`https://api.spoonacular.com/recipes/${req.params.id}/information`, {params: req.query});
-  console.log(results);
-  res.status(results.status).send(results.data) ;
-
-  });
-
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3002;
 const connectionString = process.env.MONGO_URI;
