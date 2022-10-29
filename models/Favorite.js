1414594eb86d437d80d755a4fdb5d9f87133831c
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-10-28 19:33:08 -0500

    modle file for favorites created

---
+++ models/Favorite.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+// const bcrypt = require('bcryptjs')  
+// const jwt = require('jsonwebtoken')
+// const User = require('../models/User')
+
+const FavoriteSchema = new mongoose.Schema({    
+
+    recipeId: {
+        type: String,
+        required: true,
+    },  
+    userId: { 
+
+        type: String, 
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },  
+    image: {
+        type: String,
+            
+    },
+    imageType: {
+      type: String,
+    
+    }
+  })    
+  module.exports = mongoose.model('Favorite', FavoriteSchema) 
