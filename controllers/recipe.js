f2ab2d0b6c71e90ce4057aa7c826bbc497fffac6
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-10-30 21:03:38 -0500

    added save recipe & get recipe list routes & controller logic

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,6 +1,7 @@
 const {StatusCodes} = require('http-status-codes')
 const { NotFoundError } = require('../errors')
 const User = require('../models/User')
+const Favorite = require('../models/Favorite')
 const apiKey = process.env.apiKey
 const axios = require('axios')
 
@@ -14,15 +15,34 @@ const getRecipes = async (req, res) => {
 const getRecipe = async (req, res) => {
   req.query.apiKey = apiKey;
   const results = await axios.get(`https://api.spoonacular.com/recipes/${req.params.id}/information`, {params: req.query});
-  console.log(results);
   if (!req.params.id) {
     throw new NotFoundError(`no recipe with id ${req.params.id}`)
   }
   res.status(StatusCodes.OK).send(results.data) ;
+}
+const getRecipeList = async (req, res) => {
+  console.log('got here....')
+  const favoritesList = await Favorite.find({userId: req.user.userId})
+  console.log(favoritesList);
+  res.status(StatusCodes.OK).json(favoritesList) ;
+}
+
+const saveFavorite = async (req, res) => {
+  req.body.userId = req.user.userId
+  console.log(req.user.userId)
+  const list = await Favorite.find({userId: req.user.userId, recipeId: req.body.recipeId});
+  if (list.length > 0) {
+    return res.status(StatusCodes.OK).json({message: 'Recipe already saved!'})
+  }
+  await Favorite.create(req.body)
+  res.status(StatusCodes.OK).json({message: 'Favorite recipe saved'}) ;
+
 }
 
 
 module.exports = {
   getRecipe,
   getRecipes,
+  getRecipeList,
+  saveFavorite
 }
--- routes/recipe.js
+++ routes/recipe.js
@@ -5,9 +5,12 @@ const router = express.Router()
 const { 
   getRecipes,
   getRecipe,
+  getRecipeList,
+  saveFavorite
  } = require('../controllers/recipe')
 
  router.route('/').get(getRecipes)
+ router.route('/list').get(getRecipeList).post(saveFavorite)
  router.route('/:id').get(getRecipe)
 
  module.exports = router
